e85f234002a280594d4494c5ebc09ff7b16dee1b
김현준 <hyunwns78@yonsei.ac.kr>
2022-07-19 15:39:41 +0900

    proj: 템플릿 초기화
    템플릿 작성

--- ticketing.py
+++ ticketing.py
@@ -1,1 +1,63 @@
-print('hello world')
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\kycsn\OneDrive\바탕 화면\base.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(549, 450)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QtCore.QRect(190, 400, 341, 32))
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.radioButton = QtWidgets.QRadioButton(Dialog)
+        self.radioButton.setGeometry(QtCore.QRect(330, 20, 51, 16))
+        self.radioButton.setObjectName("radioButton")
+        self.radioButton_2 = QtWidgets.QRadioButton(Dialog)
+        self.radioButton_2.setGeometry(QtCore.QRect(390, 20, 51, 16))
+        self.radioButton_2.setObjectName("radioButton_2")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(460, 20, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.comboBox = QtWidgets.QComboBox(Dialog)
+        self.comboBox.setGeometry(QtCore.QRect(40, 20, 111, 22))
+        self.comboBox.setObjectName("comboBox")
+        self.dateEdit = QtWidgets.QDateEdit(Dialog)
+        self.dateEdit.setGeometry(QtCore.QRect(40, 50, 110, 22))
+        self.dateEdit.setObjectName("dateEdit")
+        self.comboBox_2 = QtWidgets.QComboBox(Dialog)
+        self.comboBox_2.setGeometry(QtCore.QRect(180, 50, 111, 22))
+        self.comboBox_2.setObjectName("comboBox_2")
+        self.comboBox_3 = QtWidgets.QComboBox(Dialog)
+        self.comboBox_3.setGeometry(QtCore.QRect(180, 20, 111, 22))
+        self.comboBox_3.setObjectName("comboBox_3")
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.radioButton.setText(_translate("Dialog", "KTX"))
+        self.radioButton_2.setText(_translate("Dialog", "SRT"))
+        self.pushButton.setText(_translate("Dialog", "검색"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
